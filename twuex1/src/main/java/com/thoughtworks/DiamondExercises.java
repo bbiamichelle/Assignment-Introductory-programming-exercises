1756eefeeeadbc88c66b1f8a1cdf7a762d3db631
biamichelle <biamichelle@usp.br>
2018-01-22 14:55:37 -0200

    Refactor class Diamond, modify method name and variable name

--- twuex1/src/main/java/com/thoughtworks/DiamondExercises.java
+++ twuex1/src/main/java/com/thoughtworks/DiamondExercises.java
@@ -7,75 +7,72 @@ public class DiamondExercises {
     public void printIsoscelesTriangle () {
 
         Scanner scan = new Scanner(System.in);
-        System.out.print( " digite um número ");
+        System.out.print( "Enter a number");
 
-        int r = scan.nextInt();
+        int answer = scan.nextInt();
 
-        for (int n = 1; n <= r; n++) {
-            int q = getQuantidadeDeAst(n);
-            int g = getQuantidadeDeEspacos(r,n);
-            imprimiQuantidadeDeCaracter(g," ");
-            imprimiQuantidadeDeCaracter(q,"*");
+        for (int counter = 1; counter <= answer; counter++) {
+            int quantityAsterisk = getQuantityAsterisk(counter);
+            int quantitySpace = getQuantitySpace(answer,counter);
+            printQuantityCharacter(quantitySpace," ");
+            printQuantityCharacter(quantityAsterisk,"*");
             System.out.println("");
-
         }
-
     }
 
     public void printDiamond () {
 
-        int r = 3;
-        for (int n = 1; n <= r; n++) {
-            int q = getQuantidadeDeAst(n);
-            int g = getQuantidadeDeEspacos(r,n);
-            imprimiQuantidadeDeCaracter(g," ");
-            imprimiQuantidadeDeCaracter(q,"*");
+        int numberLines = 3;
+        for (int counter = 1; counter <= numberLines; counter++) {
+            int quantityAsterisk = getQuantityAsterisk(counter);
+            int quantitySpace = getQuantitySpace(numberLines,counter);
+            printQuantityCharacter(quantitySpace," ");
+            printQuantityCharacter(quantityAsterisk,"*");
             System.out.println("");
 
         }
 
-        for (int m = r - 1 ; m > 0; m--){
-            int a = getQuantidadeDeAst(m);
-            int b = getQuantidadeDeEspacos(r,m);
-            imprimiQuantidadeDeCaracter(b, " ");
-            imprimiQuantidadeDeCaracter(a, "*");
+        for (int counter = numberLines - 1 ; counter > 0; counter--){
+            int quantityAsterisk = getQuantityAsterisk(counter);
+            int quantitySpace = getQuantitySpace(numberLines,counter);
+            printQuantityCharacter(quantitySpace, " ");
+            printQuantityCharacter(quantityAsterisk, "*");
             System.out.println("");
-
         }
     }
 
     public void printDiamondWithName (){
 
-        int r = 3;
+        int numberLine = 3;
         int index = 1;
-        int incremento = 1;
+        int increase = 1;
         while(index > 0){
 
-            int q = getQuantidadeDeAst(index);
-            int g = getQuantidadeDeEspacos(r,index);
-            if(index == r){
+            int quantityAsterisk = getQuantityAsterisk(index);
+            int quantitySpace = getQuantitySpace(numberLine,index);
+            if(index == numberLine){
                 System.out.print("Bill");
-                incremento = -1;
+                increase = -1;
             } else {
-                imprimiQuantidadeDeCaracter(g," ");
-                imprimiQuantidadeDeCaracter(q,"*");
+                printQuantityCharacter(quantitySpace," ");
+                printQuantityCharacter(quantityAsterisk,"*");
             }
             System.out.println("");
 
-            index += incremento;
+            index += increase;
         }
     }
 
-    private static int getQuantidadeDeAst(int n) {
-        return 1 + 2 * (n - 1);
+    private static int getQuantityAsterisk(int numberLine) {
+        return 1 + 2 * (numberLine - 1);
     }
 
-    private static int getQuantidadeDeEspacos (int r, int n){
-        return (getQuantidadeDeAst(r) - getQuantidadeDeAst(n)) / 2;
+    private static int getQuantitySpace(int quantityBeforeWord, int quantityAfterWord){
+        return (getQuantityAsterisk(quantityBeforeWord) - getQuantityAsterisk(quantityAfterWord)) / 2;
     }
 
-    private static void imprimiQuantidadeDeCaracter(int q, String character) {
-        for (int x = 0; x < q; x ++) {
+    private static void printQuantityCharacter(int quantityCharacter, String character) {
+        for (int counter = 0; counter < quantityCharacter; counter ++) {
             System.out.print( character);
         }
     }
